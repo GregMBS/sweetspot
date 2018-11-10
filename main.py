d4c4fcb71199eabbe8219ad588995008bb75848e
Brandon <bsmith8108@gmail.com>
2018-11-10 20:35:44 +0000

    Added receiving endpoint

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-        return 'Hello, World!'
+    import pdb; pdb.set_trace()
+    return 'Hello, World!'
+
+@app.route('/receive', methods=['GET', 'POST'])
+def receive():
+    data = request.form
+    for key, value in data.items():
+        print('{}: {}'.format(key, value))
+    return 'got here'
 
 
 if __name__ == '__main__':
