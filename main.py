cf7f96569a4b6d3291dd488ce5a2f2846cc4485b
Jordon <jordon@main.us-east4-c.c.sweetspot-222116.internal>
2018-11-10 18:35:01 +0000

    added login page

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-        return 'Hello, World!'
+    return 'Hello, !'
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
