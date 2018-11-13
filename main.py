c0f8cb517933e1ae2606f1a29a3dea2eabaa7f95
Greg Blumenthal <greg@gmbs-consulting.com>
2018-11-13 12:20:11 -0600

    spacing and line-length

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask import request
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
--- models.py
+++ models.py
@@ -6,32 +6,42 @@ metadata = MetaData()
 
 t_business = Table(
     'business', metadata,
-    Column('id', BigInteger, nullable=False, unique=True, server_default=text("nextval('business_id_seq'::regclass)")),
+    Column('id', BigInteger, nullable=False, unique=True,
+           server_default=text("nextval('business_id_seq'::regclass)")),
     Column('name', Text)
 )
 
 
 t_user = Table(
     'user', metadata,
-    Column('id', BigInteger, nullable=False, unique=True, server_default=text("nextval('user_id_seq'::regclass)")),
-    Column('admin', Boolean, server_default=text("false")),
-    Column('business_id', BigInteger, nullable=False, server_default=text("nextval('user_business_id_seq'::regclass)")),
+    Column('id', BigInteger, nullable=False, unique=True,
+           server_default=text("nextval('user_id_seq'::regclass)")),
+    Column('admin', Boolean,
+           server_default=text("false")),
+    Column('business_id', BigInteger, nullable=False,
+           server_default=text("nextval('user_business_id_seq'::regclass)")),
     Column('name', Text)
 )
 
 
 t_beacon = Table(
     'beacon', metadata,
-    Column('id', BigInteger, nullable=False, unique=True, server_default=text("nextval('beacon_id_seq'::regclass)")),
-    Column('business_id', ForeignKey('business.id'), nullable=False, index=True, server_default=text("nextval('beacon_business_id_seq'::regclass)")),
-    Column('user_id', ForeignKey('user.id'), nullable=False, index=True, server_default=text("nextval('beacon_user_id_seq'::regclass)"))
+    Column('id', BigInteger, nullable=False, unique=True,
+           server_default=text("nextval('beacon_id_seq'::regclass)")),
+    Column('business_id', ForeignKey('business.id'), nullable=False, index=True,
+           server_default=text("nextval('beacon_business_id_seq'::regclass)")),
+    Column('user_id', ForeignKey('user.id'), nullable=False, index=True,
+           server_default=text("nextval('beacon_user_id_seq'::regclass)"))
 )
 
 
 t_data = Table(
     'data', metadata,
-    Column('id', BigInteger, nullable=False, unique=True, server_default=text("nextval('data_id_seq'::regclass)")),
-    Column('beacon_id', ForeignKey('beacon.id'), nullable=False, index=True, server_default=text("nextval('data_beacon_id_seq'::regclass)")),
+    Column('id', BigInteger, nullable=False, unique=True,
+           server_default=text("nextval('data_id_seq'::regclass)")),
+    Column('beacon_id', ForeignKey('beacon.id'), nullable=False, index=True,
+           server_default=text("nextval('data_beacon_id_seq'::regclass)")),
     Column('timestamp', Time),
-    Column('counter', BigInteger, server_default=text("0"))
+    Column('counter', BigInteger,
+           server_default=text("0"))
 )
